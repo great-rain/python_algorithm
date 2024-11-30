3780b4ce71fc4bce6192d8fbdf8e22b8a83fe958
JunWoo Lee <junwoo1357@gmail.com>
2024-11-30 19:01:36 +0900

    세로읽기

--- baekjoon/two_dimensional _array/세로읽기.py
+++ baekjoon/two_dimensional _array/세로읽기.py
@@ -1,21 +1,60 @@
 import sys
 # print(sys.stdin.readlines())
-a = ['AABCDD\n', 'afzz\n', '09121\n', 'a8EWg6\n', 'P5h3kx\n']# sys.stdin.readlines()
 
-r = ''
-for i in a:
-    for j in i:
-        r += j
+result = ''
+matrix = []
+max_len = 0
+for i in range(5):
+    ls = [i for i in input()]
+    max_len = max(max_len, len(ls))
+    matrix.append(ls)
 
-print(r)
+for j in range(max_len):
+    for i in range(5):
+        if len(matrix[i]) > j:
+            result += matrix[i][j]
 
-# matrix = []
-# for i in range(5):
-#     matrix.append([i for i in input()])
-#
-# for i in range(len(matrix)):
-#     for j in range(len(matrix[i])):
-#         result += matrix[j][i]
-#
-# print(result)
-#
+print(result)
+"""
+0, 0
+1, 0
+2, 0
+3, 0
+4, 0
+0, 1
+1, 1
+2, 1
+3, 1
+4, 1
+0, 2
+1, 2
+2, 2
+3, 2
+4, 2
+0, 3
+1, 3
+2, 3
+3, 3
+4, 3
+0, 4
+1, 4
+2, 4
+3, 4
+4, 4
+0, 5
+1, 5 -> X
+2, 5
+3, 5
+4, 5
+
+"""
+
+"""
+n = [input() for i in range(5)]
+
+for i in range(15):
+    for j in range(5):
+        if i < len(n[j]):
+            print(n[j][i],end='')
+
+"""
