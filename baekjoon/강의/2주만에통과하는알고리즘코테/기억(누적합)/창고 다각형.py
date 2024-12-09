d1d137c804ae82d3dc286b97092e412b202e2e76
JunWoo Lee <junwoo1357@gmail.com>
2024-12-10 00:18:12 +0900

    창고다각형 시도1

---
+++ baekjoon/강의/2주만에통과하는알고리즘코테/기억(누적합)/창고 다각형.py
@@ -0,0 +1,55 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    arr.append([x, y])
+
+# prefix
+# prefix = [[i[0] + 1, i[1]] for i in arr]
+highest_y = 0
+highest_x = 0
+width = arr[0][0] + 1
+area = arr[0][1]
+for i in range(n):
+    x, y = arr[i][0], arr[i][1]
+
+    if y > highest_y:
+        highest_y = y
+        highest_x = x
+
+print((width-highest_x) * highest_y)
+
+
+
+
+
+
+
+# 메모리얼 풀이
+# 더 작은 높이 기준으로 넓이 형성
+# 만약 다음높이가 더 작으면 -> 높이의 기준
+# 만약 다음높이가 더 크면면 -> 고점 높이의 기준
+# 넓이의 종류는 총 2개
+# - 고점인 경우
+# - 고점을 제외한 나머지 중에 제일 높은 경우. (이전 높이 중 최고점 * |(이전최고점 X - 고점 X)|
+# arr = sorted(arr)
+# area = 0
+# max_area = 0
+# height = []
+# heighest_y = 0
+# heighest_x = 0
+# for i in arr:
+#     x, y = i[0], i[1]
+#
+#     # 새로운 최고점 등장 확인
+#     if y > heighest_y:
+#         heighest_y = y
+#         heighest_x = x
+#
+#     height.append(y)
+#     area = max(height) * abs(arr[height.index(max(height))][0] - heighest_x)
+#
+# # 넓이 구하기(최고층 넓이 + 나머지 넓이)
+# print((heighest_x * heighest_y + area))
+
+# 포인터 풀이
