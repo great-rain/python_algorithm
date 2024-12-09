83f46270a7da700791565ed59892a30169e4602a
junwoo <junwoo1357@gmail.com>
2024-12-09 16:01:38 +0900

    수열 가장 크게 만들기

---
+++ baekjoon/강의/2주만에통과하는알고리즘코테/최적화(정수론)/수열 가장 크게 만들기.py
@@ -0,0 +1,14 @@
+# 1912
+# 다이나믹 프로그래밍 (메모이제이션)
+
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+prefix = [0 for _ in range(N+1)]
+
+for i in range(N):
+    prefix[i+1] = max(prefix[i] + arr[i], arr[i])
+
+prefix.pop(0)
+print(max(prefix))
