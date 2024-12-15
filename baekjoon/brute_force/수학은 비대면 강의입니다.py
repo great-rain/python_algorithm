e6e336053231c85a499dac8125b6517004b48394
JunWoo Lee <junwoo1357@gmail.com>
2024-12-15 21:54:41 +0900

    수학은 비대면 강의입니다

---
+++ baekjoon/brute_force/수학은 비대면 강의입니다.py
@@ -0,0 +1,14 @@
+a, b, c, d, e, f = map(int, input().split())
+
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            print(f'{x} {y}')
+            break
+
+"""
+a,b,c,d,e,f=map(int,input().split())
+x=(c*e-b*f)//(a*e-b*d)
+y=(c*d-a*f)//(b*d-a*e)
+print(x,y)
+"""
