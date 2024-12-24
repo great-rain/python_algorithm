b1682f94fd890b13d24776523336eb161b602d50
junwoo <junwoo1357@gmail.com>
2024-12-24 18:01:32 +0900

    소수 스니펫 범위

--- 스니펫/소수.py
+++ 스니펫/소수.py
@@ -18,3 +18,20 @@ def sieve_of_eratosthenes(limit):
         p += 1
     prime_numbers = [p for p in range(2, limit) if prime[p]]
     return prime_numbers
+
+# 범위 구하기
+prime = []
+numbers = [1, 1] + [0] * 999999
+
+for i in range(2, 1000001):
+    if numbers[i] == 0:
+        prime.append(i)
+        for j in range(2*i, 1000001, i):
+            numbers[j] = 1
+
+N, M = map(int, input().split())
+for p in prime:
+    if N <= p <= M:
+        print(p)
+    elif p > M:
+        break
