c9fbefc01442dce3919c8ab81ad28ac14957c213
junwoo <junwoo1357@gmail.com>
2025-01-23 16:22:55 +0900

    큰 수의 법칙

---
+++ 이코테/Ch03 그리디/큰 수의 법칙.py
@@ -0,0 +1,14 @@
+"""
+k번 반복 후 +1 계속 반복
+최대값 : 반복되는 경우로 묶기
+그다음 : 최대값이랑 세트로 묶임
+나머지 : 위에 과정 지나고 남은 부분도 처리 해주기(max2는 전체에서 반복수만큼 빼주기)
+"""
+n, m, k = map(int, input().split())
+numbers = sorted(map(int, input().split()))
+
+max1, max2 = numbers[-1], numbers[-2]
+count = (m // (k + 1)) * k + (m % (k + 1)) # 반복 횟수
+
+result = count * max1 + (m - count) * max2
+print(result)
