25625137c55122a2fdc13df19f2abd0aaa405a7a
junwoo <junwoo1357@gmail.com>
2025-01-23 12:34:10 +0900

    Tree (BFS) 추가

--- 스니펫/Tree.py
+++ 스니펫/Tree.py
@@ -40,7 +40,35 @@ def postorder_traversal_recursive(root):
     traverse(root)
     return result
 
+def levelOrder(root):
+    def traverse(node, level):
+        if not node:
+            return
+
+        # 레벨에 맞는 리스트가 없다면 새로 생성
+        if len(result) <= level:
+            result.append([])
+
+        # 현재 레벨에 노드 값 추가
+        result[level].append(node.val)
+
+        # 왼쪽과 오른쪽 자식에 대해 재귀 호출
+        traverse(node.left, level + 1)
+        traverse(node.right, level + 1)
+
+    result = []
+    traverse(root, 0)
+    return result
+
+
 # 트리 생성
+"""
+        1
+      /   \
+     2     3
+    / \   / \
+   4   5 6   7
+"""
 root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
@@ -53,3 +81,4 @@ root.right.right = TreeNode(7)
 print("Pre-order (Recursive):", preorder_traversal_recursive(root))  # [1, 2, 4, 5, 3, 6, 7]
 print("In-order (Recursive):", inorder_traversal_recursive(root))    # [4, 2, 5, 1, 6, 3, 7]
 print("Post-order (Recursive):", postorder_traversal_recursive(root))# [4, 5, 2, 6, 7, 3, 1]
+print("Level Order (Recursive):", levelOrder(root)) # [[1], [2, 3], [4,5,6,7]]
