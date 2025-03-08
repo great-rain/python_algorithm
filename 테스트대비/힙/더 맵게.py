84f5ae8d6e029a6a60275db43d6fa8dfd1f75f79
JunWoo Lee <junwoo1357@gmail.com>
2025-03-08 17:30:49 +0900

    더 맵게

---
+++ 테스트대비/힙/더 맵게.py
@@ -0,0 +1,13 @@
+import heapq
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    cnt = 0
+    while scoville[0] < K:
+        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2)
+        cnt += 1
+
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+    return cnt
+scoville, K =  [5, 5, 5, 5, 5], 4
+print(solution(scoville, K))
