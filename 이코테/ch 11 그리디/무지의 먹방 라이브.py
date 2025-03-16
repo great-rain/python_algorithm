354fcc717a93ed398c5e4e58e2a7bc67e1d16db1
JunWoo Lee <junwoo1357@gmail.com>
2025-03-16 22:37:44 +0900

    무지먹방 테스트케이스 추가

--- 이코테/ch 11 그리디/무지의 먹방 라이브.py
+++ 이코테/ch 11 그리디/무지의 먹방 라이브.py
@@ -10,12 +10,12 @@ def solution(food_times, k):
     q = []
     for i in range(len(food_times)):
         # (음식 시간, 음식 번호) 형태로 우선 순위 큐에 삽입
-        heapq.heappush(q, (food_times[i], i+1))
+        heapq.heappush(q, (food_times[i], i + 1))
 
-    sum_value = 0 # 먹기위해 사용한 시간
-    previous = 0 # 직전에 다 먹은 음식 시간
+    sum_value = 0  # 먹기위해 사용한 시간
+    previous = 0  # 직전에 다 먹은 음식 시간
 
-    length = len(food_times) # 남은 음식 개수
+    length = len(food_times)  # 남은 음식 개수
 
     # sum_value + (현재의 음식시간 - 이전 음식 시간) * 현재 음식 개수와 k 비교
     while sum_value + ((q[0][0] - previous) * length) <= k:
@@ -25,5 +25,11 @@ def solution(food_times, k):
         previous = now
 
     # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
-    result = sorted(q, key=lambda x: x[1]) # 음식 번호 기준으로 정렬
-    return result[(k-sum_value) % length][1]
+    result = sorted(q, key=lambda x: x[1])  # 음식 번호 기준으로 정렬
+    return result[(k - sum_value) % length][1]
+
+
+food_times = [8, 6, 4]
+k = 15
+
+print(solution(food_times, k))
